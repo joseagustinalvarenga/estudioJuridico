2ebe16bc4a6a74bbfe623f1411d999e8c9be465f
aalvarenga <aalvarenga@sancorseguros.com.ar>
2023-10-18 10:01:06 -0300

    0007

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,6 @@
-import { defineConfig } from "astro/config";
-import { astroImageTools } from "astro-imagetools";
-import mdx from "@astrojs/mdx";
-import m2dx from "astro-m2dx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import rehypeExternalLinks from "rehype-external-links";
-import fauxRemarkEmbedder from "@remark-embedder/core";
-import fauxOembedTransformer from "@remark-embedder/transformer-oembed";
-import { defineConfig } from 'astro/config';
-const remarkEmbedder = fauxRemarkEmbedder.default;
-const oembedTransformer = fauxOembedTransformer.default;
-import vue from "@astrojs/vue";
-/** @type {import('astro-m2dx').Options} */
-import prefetch from "@astrojs/prefetch";
-import compress from "astro-compress";
-import netlify from "@astrojs/netlify/functions";
-const m2dxOptions = {
-  exportComponents: true,
-  unwrapImages: true,
-  autoImports: true
-};
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://joseagustinalvarenga.github.io',
   base: '/estudioJuridico',
-});
-
+})
